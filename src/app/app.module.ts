cae020200bb2e1324086b43d623008c2fb1d5e89
Joshua Mabry <inkandcapture@gmail.com>
2017-11-13 21:36:48 -0500

    Sets up main file structure for components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './component/weather/weather.component';
+import { StocksComponent } from './component/stocks/stocks.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    StocksComponent
   ],
   imports: [
     BrowserModule
